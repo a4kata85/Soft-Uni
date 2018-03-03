2d21500a0c1ea02f5324dca368e954a8701627f3
Angel Petrov <angel_84@abv.bg>
2018-03-03 17:47:26 +0200

    Create P03. Practice-Chars-And-Strings.cs

---
+++ Courses/Programming Fundamentals/03. Data Types and Variables/P03. Practice-Chars-And-Strings.cs
@@ -0,0 +1,23 @@
+namespace P03. PracticeCharsAndStrings
+{
+    using System;
+    class StartUp
+    {
+        static void Main()
+        {
+            string first = "Software University";
+            char second = 'B';
+            char third = 'y';
+            char fourth = 'e';
+            string fifth = "I love programming";
+
+            Console.WriteLine(first);
+            Console.WriteLine(second);
+            Console.WriteLine(third);
+            Console.WriteLine(fourth);
+            Console.WriteLine(fifth);
+
+
+        }
+    }
+}
