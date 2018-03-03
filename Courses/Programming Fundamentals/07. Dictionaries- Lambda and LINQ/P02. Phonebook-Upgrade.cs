bf95d6fed978c033fb411343cb9a31e0d7e53cc1
Angel Petrov <angel_84@abv.bg>
2018-03-03 22:41:21 +0200

    Create P02. Phonebook-Upgrade.cs

---
+++ Courses/Programming Fundamentals/07. Dictionaries- Lambda and LINQ/P02. Phonebook-Upgrade.cs
@@ -0,0 +1,67 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P02.PhonebookUpgrade
+{
+    class StartUp
+    {
+        static SortedDictionary<string, string> sorted = new SortedDictionary<string, string>();
+
+        static void Main()
+        {
+            string input = Console.ReadLine();
+            while (input != "END")
+            {
+                string[] splited = input.Split(' ');
+                switch (splited[0])
+                {
+                    case "A": Add(splited[1], splited[2]); break;
+                    case "S": Find(splited[1]);break;
+                    case "ListAll": ListAll();break;                 
+                }
+                input = Console.ReadLine();
+            }
+        }
+
+        private static void Add(string name, string phone)
+        {
+            if (!sorted.ContainsKey(name))
+            {
+                sorted.Add(name, phone);
+            }
+            else
+            {
+                sorted[name] = phone;
+            }
+        }
+
+        private static void Find(string searchedName)
+        {
+            int count = 0;
+            foreach (var name in sorted)
+            {
+                if (name.Key.Equals(searchedName))
+                {
+                    Console.WriteLine("{0} -> {1}", name.Key, name.Value);
+                    count++;
+                    break;
+                }              
+            }
+            if (count == 0)
+            {
+                Console.WriteLine("Contact {0} does not exist.", searchedName);
+            }
+        }
+
+        private static void ListAll()
+        {
+            foreach (var name in sorted)
+            {
+                Console.WriteLine("{0} -> {1}", name.Key, name.Value);
+            }
+        }
+    }
+}
