c794ee6b94caae37fc3965937c5fa1fe3a6eae8b
Angel Petrov <angel_84@abv.bg>
2019-03-28 07:44:10 +0200

    Create P01. Valid Usernames.cs

---
+++ Courses/Tech Module 4.0/10. Text Processing and Regex/P01. Valid Usernames.cs
@@ -0,0 +1,41 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace P01._Valid_Usernames
+{
+    class StartUp
+    {
+        static void Main()
+        {
+            string[] words = Console.ReadLine().Split(", ").ToArray();
+            
+            for (int i = 0; i < words.Length; i++)
+            {
+                string word = words[i];
+                bool isValid = false;
+
+                if (word.Length>=3 && word.Length<=16)
+                {
+                    for (int j = 0; j < word.Length; j++)
+                    {
+                        char currentChar = word[j];
+                        if (char.IsLetterOrDigit(currentChar) || currentChar == '-' || currentChar == '_')
+                        {
+                            isValid = true;
+                        }
+                        else
+                        {
+                            isValid = false;
+                            break;
+                        }
+                    }
+                }
+                if (isValid)
+                {
+                    Console.WriteLine(word);
+                }
+            }
+        }
+    }
+}
